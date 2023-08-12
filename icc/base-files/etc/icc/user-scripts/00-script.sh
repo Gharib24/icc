37fd1fe98acd4ff00b5c81770fbecf29d27cd978
Ahmad Gharib <ahmad2484@gmail.com>
2023-08-12 14:40:48 +0100

    first commit

---
+++ icc/base-files/etc/icc/user-scripts/00-script.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+[ "$LED" = "none" ] && {
+	case $SERVICE_ACTION in
+		start)
+			/usr/bin/mt1300_led off
+			/usr/bin/mt1300_led blue_breath daemon
+		;;
+		stop)
+			/etc/init.d/led start
+			exit 0
+		;;
+		reload)
+			:
+		;;
+		running)
+			:
+		;;
+	esac
+	
+	case $STATUS in
+		online)
+			/usr/bin/mt1300_led white daemon
+		;;
+		offline)
+			/usr/bin/mt1300_led white_breath daemon
+		;;
+		unknown)
+			:
+		;;
+		panic)
+			:
+		;;
+	esac
+}
+
+case $STATUS in
+	online)
+		:
+	;;
+	offline)
+	:
+		/usr/bin/btwifi.login
+	;;
+	unknown)
+		:
+	;;
+esac
